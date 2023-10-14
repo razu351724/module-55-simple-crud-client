f6f65125826322410c35c2e570e60489770e8aac
RAZU-sheikh <razu351724@gmail.com>
2023-10-14 22:34:24 +0600

    The user email and password cullect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
 
 function App() {
-
+  const handleAddUser = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    console.log(name, email)
+  }
 
   return (
     <>
 
       <h1>Simple CRUD</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder='Type your name' />
+        <br />
+        <input type="email" name="email" placeholder='Type your email'/>
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
 
     </>
   )
