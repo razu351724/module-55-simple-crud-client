15670b6d0086ffdac2984273c2917b671a7a1851
RAZU-sheikh <razu351724@gmail.com>
2023-10-16 00:36:50 +0600

    update an data user and server side show

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -9,6 +9,22 @@ const Update = () => {
         const name = form.name.value;
         const email = form.email.value;
         console.log(name, email);
+        const updatedUser = {name, email};
+
+        fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.modifedCount>0){
+                alert('user updated successfully')
+            }
+        })
     }
 
     return (
